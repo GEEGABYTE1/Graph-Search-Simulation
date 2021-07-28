a51fba0909a30bf01073728a9e78871e86db4007
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-07-27 22:50:49 -0400

    Graph Built

--- test_values.py
+++ test_values.py
@@ -18,9 +18,29 @@ test_graph.add_vertex(four)
 test_graph.add_vertex(five)
 test_graph.add_vertex(six)
 
+# Test Graph #
+
+test_graph.add_edge(one, two)
+test_graph.add_edge(one, three)
+test_graph.add_edge(one, five)
+test_graph.add_edge(two, three)
+test_graph.add_edge(two, one)
+test_graph.add_edge(three, two)
+test_graph.add_edge(three, four)
+test_graph.add_edge(three, five)
+test_graph.add_edge(four, six)
+test_graph.add_edge(four, five)
+test_graph.add_edge(five, three)
+test_graph.add_edge(five, four)
+test_graph.add_edge(six, four)
+test_graph.add_edge(six, five)
+
+
+
 dictionary = {}
 
 for vertex, edges in test_graph.graph_dict.items():
     edges = list(edges.edges.keys())
     dictionary[vertex] = edges 
 
+print(dictionary)
