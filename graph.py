7c997530cf524fff5021a4e04d7038577d53a258
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-07-27 21:33:03 -0400

    Initial Files

---
+++ graph.py
@@ -0,0 +1,17 @@
+from vertex import Vertex 
+
+class Graph:
+    def __init__(self, directed=None):
+        self.directed = directed 
+        self.graph_dict = {}
+
+    def add_vertex(self, vertex):
+        self.graph_dict[vertex.value] = vertex 
+
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+
+        if not self.directed:
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+        
+
---
+++ test_values.py
@@ -0,0 +1,21 @@
+from graph import Graph 
+from vertex import Vertex 
+
+
+test_graph = Graph()
+
+one = Vertex(1)
+two = Vertex(2)
+three = Vertex(3)
+four = Vertex(4)
+five = Vertex(5)
+six = Vertex(6)
+
+test_graph.add_vertex(one)
+test_graph.add_vertex(two)
+test_graph.add_vertex(three)
+test_graph.add_vertex(four)
+test_graph.add_vertex(five)
+test_graph.add_vertex(six)
+
+
