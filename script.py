62582fd4efc9c4c8f3662a293c0d3ec0c877cb92
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-07-28 22:36:05 -0400

    Dfs

--- script.py
+++ script.py
@@ -19,34 +19,25 @@ class Script:
         print('1: View the Graph Dictionary')
         print('2: Simulate the Breadth-First Graph Search')
         print('3: Simulate the Depth-First Graph Search')
+        print('/quit: Quit the simulation')
 
+        playing = True
+        while playing:
+            prompt = int(input(':'))
+            print('\n')
 
-        prompt = int(input(':'))
-        print('\n')
+            if prompt == 1:
+                for key, value in self.dictionary.items():
+                    print(str(key) + ' ---> ' + str(value))
+            elif prompt == 2:
+                self.bfs_implementation()
 
-        if prompt == 1:
-            for key, value in self.dictionary.items():
-                print(str(key) + ' ---> ' + str(value))
-        elif prompt == 2:
-            self.bfs_implementation()
-
-        elif prompt == 3:
-            print('Please enter a number that is between 1 and 6')
-            player = True
-            while player:
-                start = int(input('Enter a number you want to start the path with: '))
-                if start > 6:
-                    print("That does not seem like a valid number")
-                else:
-                    break 
-            while player:
-                end = int(input('Enter a number you would like to find a path to: '))
-                if end > 6:
-                    print('That does not seem like a valid number ')
-                else:
-                    break
+            elif prompt == 3:
+                self.dfs_implementation()
             
-            print(dfs(self.dictionary, start, end))
+            elif prompt == '/quit':
+                break
+            
 
     def bfs_implementation(self):
         print('Please enter a number that is between 1 and 6')
@@ -76,6 +67,40 @@ class Script:
                 current_edge = self.dictionary[start]
                 self.dictionary[start].pop(0)
                 output = bfs(self.dictionary, start, end)
+                if output == None:
+                    break 
+                else:
+                    print(output)
+            else:
+                break
+    
+    def dfs_implementation(self):
+        print('Please enter a number that is between 1 and 6')
+        player = True
+        while player:
+            start = int(input('Enter a number you want to start the path with: '))
+            if start > 6:
+                print("That does not seem like a valid number")
+            else:
+                break 
+        while player:
+            end = int(input('Enter a number you would like to find a path to: '))
+            if end > 6:
+                print('That does not seem like a valid number ')
+            else:
+                break
+        
+        print(dfs(self.dictionary, start, end))
+        print()
+        time.sleep(0.1)
+        running = True 
+        while running:
+            prompt2 = input("Would you like to see another path? Enter y/n: ")
+
+            if prompt2 == 'y':
+                current_edge = self.dictionary[start]
+                self.dictionary[start].pop(0)
+                output = dfs(self.dictionary, start, end)
                 if output == None:
                     break 
                 else:
